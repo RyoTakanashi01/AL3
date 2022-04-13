329a7fa63b4bdca071910449759186c46cb2999b
RyoTakanashi01 <K019G1339@g.neec.ac.jp>
2022-04-13 15:35:18 +0900

    ソース

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,10 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +17,43 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	//ファイル名を指定してテクスチャ読み込み
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
+	//モデル生成
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	//サウンドデータ読み込み
+	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+	audio_->SetVolume(soundDataHandle_, 0.1f);
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { 
+
+	//スプライトの今の座標を取得
+
+	XMFLOAT2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	sprite_->SetPosition(position);
+
+	if (input_->TriggerKey(DIK_SPACE)) {
+		audio_->StopWave(voiceHandle_);
+	}
+	value_++;
+	//旧式
+	std::string strDebug = std::string("Value:") + std::to_string(value_);
+	debugText_->Print(strDebug, 50, 50, 1.0f);
+
+	//新仕様
+	//debugText_->Printf("Value:%d", value_, 50, 50, 1.0f);
+}
 
 void GameScene::Draw() {
 
@@ -43,6 +80,9 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
+	
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+
 	/// </summary>
 
 	// 3Dオブジェクト描画後処理
@@ -55,6 +95,9 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
+
+	sprite_->Draw();
+
 	/// </summary>
 
 	// デバッグテキストの描画
